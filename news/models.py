79cf0c078ed74d0f7aad52e7796476174019f102
tshap88 <tshap88@gmail.com>
2014-04-16 18:10:06 -0700

    method save() work is fine

--- news/models.py
+++ news/models.py
@@ -2,7 +2,7 @@ import os
 import urllib
 import urllib2
 import urlparse
-from django.core.files.base import ContentFile, File
+from django.core.files.base import ContentFile
 from django.db import models
 import datetime
 from django.utils import timezone
@@ -15,11 +15,9 @@ class News(models.Model):
     link = models.URLField(blank=True)
     img_file = models.ImageField('file name', upload_to='imgs/', blank=True)
     img_url = models.URLField(blank=True, null=True)
-   # image = models.F
+
     def __unicode__(self):
         return self.name
-
-
 
     def save(self):
         if self.img_url:
@@ -31,4 +29,4 @@ class News(models.Model):
                 ContentFile(image_data.read()),
                 save=False
             )
-            super(News, self).save()
+        super(News, self).save()
