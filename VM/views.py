72f9ec4bd3e4d6a281700abc11ba85b8d63ef095
tshap88 <tshap88@gmail.com>
2014-07-25 16:15:23 -0700

    replace News.objects.all() on news

--- VM/views.py
+++ VM/views.py
@@ -6,7 +6,6 @@ import datetime
 #from app1.models import App1,Choice
 from news.models import News
 
-
 def index(request):
     now = datetime.datetime.now()
     news = News.objects.all()
@@ -20,28 +19,28 @@ def index(request):
 
 def support(request):
     news = News.objects.all()
-    news_bar = News.objects.all().order_by('date_pub')#[len(news)-5:len(news):-1]
+    news_bar = news.order_by('date_pub')#[len(news)-5:len(news):-1]
     context =  {'news_bar': news_bar}
     return render(request, 'support.html', context)
 
 
 def about(request):
     news = News.objects.all()
-    news_bar = News.objects.all().order_by('date_pub')#[len(news)-5:len(news):-1]
+    news_bar = news.order_by('date_pub')#[len(news)-5:len(news):-1]
     context =  {'news_bar': news_bar}
     return render(request, 'about.html', context)
 
 
 def blog(request):
     news = News.objects.all()
-    news_bar = News.objects.all().order_by('date_pub')#[len(news)-5:len(news):-1]
+    news_bar = news.order_by('date_pub')#[len(news)-5:len(news):-1]
     context =  {'news_bar': news_bar}
     return render(request, 'blog.html', context)
 
 
 def contact(request):
     news = News.objects.all()
-    news_bar = News.objects.all().order_by('date_pub')[len(news)-5:len(news):-1]
+    news_bar = news.order_by('date_pub')[len(news)-5:len(news):-1]
     context =  {'news_bar': news_bar}
     return render(request, 'contact.html', context)
 
